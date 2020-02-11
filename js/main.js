e515621b3966ece87dacb6dd751fa1c3189b8f11
Eric Peterson <ericjamesp.2009@gmail.com>
2020-02-11 13:48:35 -0600

    Working on Jackpot...

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //CONSTANTS
-const images = ['cherries.png', 'bell.png'] ;
+const images = [ '7.png']
 const jackpot = 10;
 const win = 7;
 const bet = 5;
@@ -10,25 +10,26 @@ const goAudio = new Audio('118239__pierrecartoons1979__slot-machine-phrygian.mp3
 const reelAudio = new Audio('337079__tieswijnen__bicycle-spokes.aiff');
 // /*----- app's state (variables) -----*/
 var state = {
-    money: 0,
-    bet: 0,
-    betMax: 0
+    money: '',
+    bet: '',
+    betMax: ''
 };
 var slot123 = {
     '1': {
-        imgUrl: 'leslie.png'
+        imgUrl: ''
     },
     '2': {
-        imgUrl: 'cherries.png'
+        imgUrl: ''
     },
     '3': {
-        imgUrl: 'bell.png'
+        imgUrl: ''
     }
 };
 
 /*----- cached element references -----*/
 var totalMessage = document.getElementById('current-balance');
 var dollarInput = document.getElementById('input');
+
 
 
 
@@ -44,23 +45,40 @@ document.getElementById('numberBtn').addEventListener('click', getDollars);
 
 
 /*----- functions -----*/
+init();
+
 function init () {
     totalMessage.innerText= 'Welcome!';
-    console.log(totalMessage);
+    state = {
+        money: 0,
+        bet: 0,
+        betMax: 0
+    };
+    slot123 = {
+        '1': {
+            imgUrl: ''
+        },
+        '2': {
+            imgUrl: ''
+        },
+        '3': {
+            imgUrl: ''
+        }
+    };
 
 }
 
 function render() {
-
+    nan();
     if (state.money >= 5) {
         spin();
     }
     else {
         totalMessage.innerText= 'Min bet is $5.00.';
-    }
+    }    
 }
 function renderMax() {
-
+    nan();
     if (state.money >= 20) {
         spinMax();
     }
@@ -72,23 +90,25 @@ function spin() {
     state.bet = 0;
     state.money -= 5;
     state.bet = 5;
+    nan();
     goAudio.play();
-    renderCurrentDollars();
     renderSlots();
+    clearSlotData();
 }
 function spinMax() {
     state.betMax = 0;
     state.money -= 20;
     state.betMax = 20;
+    nan();
     goAudio.play();
-    renderCurrentDollars();
     renderSlots();
+    clearSlotData();
 }
 function renderSlots () {
     for (let i = 1; i <= 3; i++){
         //two args time/index
         handleTime(`${i}000`, i);
-    }
+    }  
 }
 function randomizeImg() {
     var image = images[Math.floor(Math.random()*images.length)];
@@ -104,33 +124,42 @@ function renderRandomImage(x){
 function handleTime(time, index) {
     setTimeout(() => {
         renderRandomImage(index);
-        if ( checkForWin() === true ) {
-            renderWin();
-        }
         // goAudio.pause();
-        renderCurrentDollars();
+        totalMessage.innerText = 'SPINNING...';
+        if (checkForWin() === true) {
+            if (checkForJackpot() === true) {
+                renderJackpot();
+            }
+            else {
+                renderWin();
+            }
+        }
     }, Number(time));
-    totalMessage.innerText = 'SPINNING...';
+ 
 }
 
 function getDollars() {
+    nan();
     state.money += parseInt(dollarInput.value);
     renderCurrentDollars();
     dollarInput.value = '';
 }
  
 function renderCurrentDollars() {
+    nan();
     totalMessage.innerText= '$ ' + state.money + '.00';
 }
 
 function renderWin() {
     totalMessage.innerText = 'Winner!!!!!';
     state.bet *= 10;
-    state.betMax *= 20;
+    state.betMax *= 10;
     state.money += state.bet;
     state.money += state.betMax;
     state.bet = 0;
     state.betMax = 0;
+    renderCurrentDollars();
+
 }
 
 function checkForWin() {
@@ -140,6 +169,43 @@ function checkForWin() {
     else {
         return false;
     }
-
+}
+function checkForJackpot() {
+    if (slot123[1] === "file:///Users/apple/code/slot-machine/7.png") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function nan() {
+    if (isNaN(state.money) === true) {
+        state.money = 0;
+        totalMessage.innerText = 'Please add more funds...';
+    }
 }
 
+function renderJackpot() {
+    totalMessage.innerText = 'JACKPOT!!!';
+    state.bet *= 50;
+    state.betMax *= 50;
+    state.money += state.bet;
+    state.money += state.betMax;
+    state.bet = 0;
+    state.betMax = 0;
+    renderCurrentDollars();
+}
+
+function clearSlotData() {
+    slot123 = {
+        '1': {
+            imgUrl: ''
+        },
+        '2': {
+            imgUrl: ''
+        },
+        '3': {
+            imgUrl: ''
+        }
+    };
+}
